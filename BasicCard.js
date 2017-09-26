2bf6551cf15a5f2e00562bf8624a24dfc03bbebb
Ciarra Murphy <mciarra@ymail.com>
2017-09-26 18:03:30 -0400

    added inquirer

---
+++ BasicCard.js
@@ -0,0 +1,8 @@
+console.log("I've loaded successfully");
+//questions for the flashcards need to be in a constructor
+
+//answers for the questions need to be in a constructor
+
+
+
+module.exports = BasicCard;
