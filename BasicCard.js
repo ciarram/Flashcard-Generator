d2f398c6d72259ba6117a64359c1e1824491933e
Ciarra Murphy <mciarra@ymail.com>
2017-09-26 23:58:24 -0400

    fixed questions and answers in BasicCard function

--- BasicCard.js
+++ BasicCard.js
@@ -4,11 +4,20 @@
 
 
 function BasicCard(front,back){
-    this.front = question;
-    this.back = answer;
+    this.front = front;
+    this.back = back;
 }
 
-
-
-
+var firstQuestion = new BasicCard("When did the Spanish Armada attempt to attack England?", 1588);
+console.log(firstQuestion);
+var secondQuestion = new BasicCard("Which French monarch was known as the 'Sun King'?", "Louis XIV");
+console.log(secondQuestion);
+var thirdQuestion = new BasicCard("The Ottoman Empire captured which city in 1453?", "Constantinople");
+console.log(thirdQuestion);
+var fourthQuestion = new BasicCard("In 1066, William the Conqurer fought in which battle to gain control of the English crown?", "Battle of Hastings");
+console.log(fourthQuestion);
+var fifthQuestion = new BasicCard("What was the fight between the Lancaster and York houses for the English crown?", "War of the Roses");
+console.log(fifthQuestion);
+var sixthQuestion = new BasicCard("What was the name of the Native American city that existed in current day St. Louis?", "Cahokia");
+console.log(sixthQuestion);
 module.exports = BasicCard;
